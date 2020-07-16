be76be49f10b1ec0939ce34cf78e1de720f1e6f8
hpulegion <hpulegion@163.com>
2020-07-16 21:23:23 +0800

    登录页面demo

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+//引入完整的elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+//或者按需引入，详情见饿了么文档
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Login/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id='login'>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <el-row style="display:flex;justify-content:center;">
+            <el-button type="primary" plain>登录</el-button>
+            <el-button type="primary" plain>注册</el-button>
+          </el-row>
+        </div>
+        <div>
+            <div class="box">
+                <div class="boxItem">邮箱：</div>
+                <el-input
+                  placeholder="请输入邮箱"
+                  v-model="emailValue"
+                  clearable>
+                </el-input>
+            </div>
+            <div class="box">
+                <div class="boxItem">密码：</div>
+                <el-input
+                  placeholder="请输入密码"
+                  v-model="passWordValue"
+                  clearable>
+                </el-input>
+            </div>
+            <div class="box">
+                <div class="boxItem">验证码：</div>
+                <el-row style="display:flex;">
+                  <el-input
+                    placeholder="请输入验证码"
+                    v-model="codeValue"
+                    clearable
+                    style="margin-right:20px;">
+                  </el-input>
+                  <el-button type="primary">主要按钮</el-button>
+                </el-row>
+            </div>
+            <div class="loginSubmit">
+                <span>登录</span>
+            </div>
+        </div>
+      </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      emailValue:'',
+      passWordValue:'',
+      codeValue:'',
+
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang='scss'>
+  #login{
+    height: 100vh;
+    background-color: #2e4e6e;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 480px;
+  }
+  .loginSubmit{
+    background-color: #00ace6;
+    height: 42px;
+    border-radius: 3px;
+    text-align: center;
+    line-height: 42px;
+    font-size: 19px;
+    color: #fff;
+  }
+  .box{
+    margin-bottom: 10px;
+  }
+  .boxItem{
+    margin-bottom: 5px;
+  }
+</style>
