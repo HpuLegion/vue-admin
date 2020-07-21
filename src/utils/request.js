c8e4ddf19f710dc5864f6669218fef6d8f918013
hpulegion <hpulegion@163.com>
2020-07-21 22:26:06 +0800

    请求拦截器中加入请求头

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,6 +11,9 @@ const instance=axios.create({
 // 添加请求拦截器
 instance.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
+    // console.log(config)
+    config.headers.token='nihao'
+    // config.headers.['token']='nihao'
     return config;
   }, function (error) {
     // 对请求错误做些什么
